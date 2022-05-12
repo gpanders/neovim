a01edb78bb8fc00e032fc1f2d697ad01ebde937a
Gregory Anders <greg@gpanders.com>
2022-05-12 13:07:37 -0600

    chore: quick update, squash later

--- src/nvim/options.lua
+++ src/nvim/options.lua
@@ -1249,6 +1249,13 @@ return {
       defaults={if_true=false}
     },
     {
+      full_name='insertmode', abbreviation='im',
+      short_desc=N_("start the edit of a file in Insert mode"),
+      type='bool', scope={'global'},
+      varname='p_force_off',
+      defaults={if_true=false}
+    },
+    {
       full_name='isfname', abbreviation='isf',
       short_desc=N_("characters included in file names and pathnames"),
       type='string', list='comma', scope={'global'},
